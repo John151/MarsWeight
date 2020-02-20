b8ced65f74cd8ae455562c0fd6b54d0f060239be
JC <blahblahblah@email.com>
2020-02-20 11:11:37 -0600

    Add project files.

---
+++ MarsWeight/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace MarsWeight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWeightEarth = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWeightMars = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtObject = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Weight on Earth";
+            // 
+            // txtWeightEarth
+            // 
+            this.txtWeightEarth.Location = new System.Drawing.Point(166, 55);
+            this.txtWeightEarth.Name = "txtWeightEarth";
+            this.txtWeightEarth.Size = new System.Drawing.Size(175, 20);
+            this.txtWeightEarth.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Weight on Mars";
+            // 
+            // txtWeightMars
+            // 
+            this.txtWeightMars.Location = new System.Drawing.Point(166, 98);
+            this.txtWeightMars.Name = "txtWeightMars";
+            this.txtWeightMars.ReadOnly = true;
+            this.txtWeightMars.Size = new System.Drawing.Size(175, 20);
+            this.txtWeightMars.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(65, 146);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Name of object";
+            // 
+            // txtObject
+            // 
+            this.txtObject.Location = new System.Drawing.Point(166, 19);
+            this.txtObject.MaxLength = 50;
+            this.txtObject.Name = "txtObject";
+            this.txtObject.Size = new System.Drawing.Size(175, 20);
+            this.txtObject.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(367, 192);
+            this.Controls.Add(this.txtObject);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtWeightMars);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtWeightEarth);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Mars Weight";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWeightEarth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWeightMars;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtObject;
+    }
+}
+
